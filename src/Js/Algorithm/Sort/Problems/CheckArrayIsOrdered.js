350897bd22872f0c90b2cd3ce0b78935af17e946
Ajith27887 <ajithyogeshcoc@gmail.com>
2025-04-21 22:57:18 +0530

    checkarraysorder - completed

--- src/Js/Algorithm/Sort/Problems/CheckArrayIsOrdered.js
+++ src/Js/Algorithm/Sort/Problems/CheckArrayIsOrdered.js
@@ -1,32 +1,11 @@
-// const Solution = ( arr, n ) => {
-// 	let i = 0;
-//     if(arr[i -1] < arr[i]){         			
-// 		console.log("true");
-//         return true;
-//     }else{
-// 		console.log("false");
-//         return false;
-//     }
-// }
-
-
 const Solution = (arr) => {
-    let count = 0;
-    let n = arr.length;
-
-    for (let i = 0; i < n; i++) {
-		console.log(arr[(i + 1) % n], "div");
-		
-        if (arr[i] > arr[(i + 1) % n]) {
-            count++;
-        }
-    }
-
-    return count <= 1;
+	let count = 0;
+	for (let i = 0; i < arr.length; i++) {
+	  if (arr[i] > arr[(i + 1) % arr.length]) {
+		count++;
+	  }
+	}
+	console.log(count <= 1, count);
 }
+
 Solution([3,4,5,1,2]);
-
-// 3 < 4 = true; 
-// 4 < 5 = true;
-// 5 < 1 = false;
-
